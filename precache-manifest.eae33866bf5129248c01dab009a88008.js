bc655524cc54c4a215777aaad0c0535920d832d3
Brian Bartholomew <bartholomewbd@gmail.com>
2019-04-10 11:25:18 -0400

    Updates

---
+++ precache-manifest.eae33866bf5129248c01dab009a88008.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2ed4794c11051dbbdded",
+    "url": "/kittysocial/static/css/main.22ff8d71.chunk.css"
+  },
+  {
+    "revision": "2ed4794c11051dbbdded",
+    "url": "/kittysocial/static/js/main.2e01361b.chunk.js"
+  },
+  {
+    "revision": "2c46a5d31f0713d01ed6",
+    "url": "/kittysocial/static/js/runtime~main.000468e4.js"
+  },
+  {
+    "revision": "2fadf87f3697dd38db8d",
+    "url": "/kittysocial/static/css/2.a07ed53c.chunk.css"
+  },
+  {
+    "revision": "2fadf87f3697dd38db8d",
+    "url": "/kittysocial/static/js/2.1a543e43.chunk.js"
+  },
+  {
+    "revision": "223e1ff88ef0f00ea1c4fb75e4a23481",
+    "url": "/kittysocial/index.html"
+  }
+];
